abb099c91a5cdb23c50aab62b0e63ca768310a1d
tiffany <tiffany.pelkey@uvm.edu>
2024-10-02 10:24:49 -0400

    Cat

---
+++ index.js
@@ -0,0 +1,3 @@
+//properties - color=black, name=spooky, age=2years, hair length=short//
+//events - walk, eat, sleep, hunt//
+//methods - eating or sleeping increases health, walking or hunting decreases health//
